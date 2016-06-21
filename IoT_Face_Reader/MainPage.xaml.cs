ff5cc5148bf3962e1893467c2088e44ef66a3179
tyler <yostx038@umn.edu>
2016-06-21 12:02:22 -0500

    working on UI

--- IoT_Face_Reader/MainPage.xaml.cs
+++ IoT_Face_Reader/MainPage.xaml.cs
@@ -7,6 +7,11 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
@@ -22,14 +27,137 @@ namespace IoT_Face_Reader
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        private MediaCapture mediaCapture;
+        private StorageFile photoFile;
+        private readonly string PHOTO_FILE_NAME = "photo.jpg";
+        private bool isPreviewing;
+
+        #region HELPER_FUNCTIONS
+
+        enum Action
+        {
+            ENABLE,
+            DISABLE
+        }
+        /// <summary>
+        /// Helper function to enable or disable Initialization buttons
+        /// </summary>
+        /// <param name="action">enum Action</param>
+        private void SetInitButtonVisibility(Action action)
+        {
+            if (action == Action.ENABLE)
+            {
+                video_init.IsEnabled = true;
+                // audio_init.IsEnabled = true;
+            }
+            else
+            {
+                video_init.IsEnabled = false;
+                // audio_init.IsEnabled = false;
+            }
+        }
+
+        /// <summary>
+        /// Helper function to enable or disable video related buttons (TakePhoto, Start Video Record)
+        /// </summary>
+        /// <param name="action">enum Action</param>
+        private void SetVideoButtonVisibility(Action action)
+        {
+            if (action == Action.ENABLE)
+            {
+                takePhoto.IsEnabled = true;
+                takePhoto.Visibility = Visibility.Visible;
+
+                // recordVideo.IsEnabled = true;
+                // recordVideo.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                takePhoto.IsEnabled = false;
+                takePhoto.Visibility = Visibility.Collapsed;
+
+                // recordVideo.IsEnabled = false;
+                // recordVideo.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        #endregion
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            SetInitButtonVisibility(Action.ENABLE);
+            SetVideoButtonVisibility(Action.DISABLE);
+            // SetAudioButtonVisibility(Action.DISABLE);
+
+            // isRecording = false;
+            isPreviewing = false;
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private async void initVideo_Click(object sender, RoutedEventArgs e)
         {
+            // Disable all buttons until initialization completes
 
+            SetInitButtonVisibility(Action.DISABLE);
+            SetVideoButtonVisibility(Action.DISABLE);
+            // SetAudioButtonVisibility(Action.DISABLE);
+
+            try
+            {
+                if (mediaCapture != null)
+                {
+                    // Cleanup MediaCapture object
+                    if (isPreviewing)
+                    {
+                        await mediaCapture.StopPreviewAsync();
+                        captureImage.Source = null;
+                        playbackElement.Source = null;
+                        isPreviewing = false;
+                    }
+                    //if (isRecording)
+                   // {
+                   //     await mediaCapture.StopRecordAsync();
+                    //    isRecording = false;
+                    //    recordVideo.Content = "Start Video Record";
+                    //    recordAudio.Content = "Start Audio Record";
+                    //}
+                    mediaCapture.Dispose();
+                    mediaCapture = null;
+                }
+
+                statusBox.Text = "Initializing camera...";
+                // Use default initialization
+                mediaCapture = new MediaCapture();
+                await mediaCapture.InitializeAsync();
+
+                // Set callbacks for failure and recording limit exceeded
+                statusBox.Text = "Device successfully initialized for video recording!";
+                mediaCapture.Failed += new MediaCaptureFailedEventHandler(mediaCapture_Failed);
+                mediaCapture.RecordLimitationExceeded += new Windows.Media.Capture.RecordLimitationExceededEventHandler(mediaCapture_RecordLimitExceeded);
+
+                // Start Preview                
+                previewElement.Source = mediaCapture;
+                await mediaCapture.StartPreviewAsync();
+                isPreviewing = true;
+                statusBox.Text = "Camera preview succeeded";
+
+                // Enable buttons for video and photo capture
+                SetVideoButtonVisibility(Action.ENABLE);
+
+                // Enable Audio Only Init button, leave the video init button disabled
+                // audio_init.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                statusBox.Text = "Unable to initialize camera for audio/video mode: " + ex.Message;
+            }
+        }
+
+        private void ClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            this.HelloMessage.Text = "Hello, Windows IoT Core!";
         }
     }
 }
