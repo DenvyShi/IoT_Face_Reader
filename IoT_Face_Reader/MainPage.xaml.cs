2134388660a76f534bb5691885ecd13db61f5c72
tyler <yostx038@umn.edu>
2016-07-01 14:59:14 -0500

    face and emotion APIs working

--- IoT_Face_Reader/MainPage.xaml.cs
+++ IoT_Face_Reader/MainPage.xaml.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Drawing;
+using System.Net;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
+using Windows.Graphics.Imaging;
 using Windows.Media.Capture;
 using Windows.Media.MediaProperties;
 using Windows.Storage;
@@ -17,6 +22,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Face.Contract;
+using System.Net.Http;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -27,6 +36,8 @@ namespace IoT_Face_Reader
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        // TAY's Face A free key from MSFT Cog Svcs.
+        private readonly IFaceServiceClient faceServiceClient = new FaceServiceClient("9725d03742394560be3ff295e1e435a2");
 
         private MediaCapture mediaCapture;
         private StorageFile photoFile;
@@ -88,6 +99,7 @@ namespace IoT_Face_Reader
         public MainPage()
         {
             this.InitializeComponent();
+            SynchronizationContext syncContext = SynchronizationContext.Current;
 
             SetInitButtonVisibility(Action.ENABLE);
             SetVideoButtonVisibility(Action.DISABLE);
@@ -124,7 +136,7 @@ namespace IoT_Face_Reader
         }
 
 
-        private async void initVideo_Click(object sender, RoutedEventArgs e)
+        public async void initVideo_Click(object sender, RoutedEventArgs e)
         {
             // Disable all buttons until initialization completes
 
@@ -134,6 +146,9 @@ namespace IoT_Face_Reader
 
             try
             {
+                // still working on NMEA
+                // updateNMEA();
+
                 if (mediaCapture != null)
                 {
                     // Cleanup MediaCapture object
@@ -145,8 +160,8 @@ namespace IoT_Face_Reader
                         isPreviewing = false;
                     }
                     //if (isRecording)
-                   // {
-                   //     await mediaCapture.StopRecordAsync();
+                    // {
+                    //     await mediaCapture.StopRecordAsync();
                     //    isRecording = false;
                     //    recordVideo.Content = "Start Video Record";
                     //    recordAudio.Content = "Start Audio Record";
@@ -197,8 +212,9 @@ namespace IoT_Face_Reader
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private async void takePhoto_Click(object sender, RoutedEventArgs e)
+        public async void takePhoto_Click(object sender, RoutedEventArgs e)
         {
+
             try
             {
                 takePhoto.IsEnabled = false;
@@ -216,10 +232,47 @@ namespace IoT_Face_Reader
                 BitmapImage bitmap = new BitmapImage();
                 bitmap.SetSource(photoStream);
                 captureImage.Source = bitmap;
+
+                // and now for the face API call
+                // FaceAttributes attribs = await UploadAndDetectFaces(photoFile.Path);
+                statusBox.Text = "00 ok just about to upload face";
+                // Emotion A key: c9306a1f134749759f1f4f9ae8838e1a
+
+                //var photoStreamNonrandom = new StreamReader(photoStream.AsStream());
+                Stream stream = photoStream.AsStream();
+                Stream fs1 = await photoFile.OpenStreamForReadAsync();
+                Stream fs2 = await photoFile.OpenStreamForReadAsync();
+
+                String faceURL = "http://i234.photobucket.com/albums/ee136/suwarnaadi/hair/Jason-Bateman-formal-hairstyle.jpg";
+
+                var faceClient = new FaceServiceClient("9725d03742394560be3ff295e1e435a2");
+                var emotionClient = new EmotionServiceClient("c9306a1f134749759f1f4f9ae8838e1a");
+                var result = await faceClient.DetectAsync(fs1);
+                var emotionResult = await emotionClient.RecognizeAsync(fs2);
+
+                // the above result returns a Face[], so...
+                ageBox.Text = result[0].FaceAttributes.Age.ToString();
+                genderBox.Text = result[0].FaceAttributes.Gender.ToString();
+                smileBox.Text = result[0].FaceAttributes.Smile.ToString();
+                // facialHairBox.Text = result[0].FaceAttributes.FacialHair.ToString();
+                glassesBox.Text = result[0].FaceAttributes.Glasses.ToString();
+
+                angerBox.Text = emotionResult[0].Scores.Anger.ToString();
+                contemptBox.Text = emotionResult[0].Scores.Contempt.ToString();
+                disgustBox.Text = emotionResult[0].Scores.Disgust.ToString();
+                fearBox.Text = emotionResult[0].Scores.Fear.ToString();
+                happinessBox.Text = emotionResult[0].Scores.Happiness.ToString();
+                neutralBox.Text = emotionResult[0].Scores.Neutral.ToString();
+                sadnessBox.Text = emotionResult[0].Scores.Sadness.ToString();
+                surpriseBox.Text = emotionResult[0].Scores.Surprise.ToString();
+
+                
+
+
             }
             catch (Exception ex)
             {
-                statusBox.Text = ex.Message;
+                statusBox.Text = "foo: " + ex.Message;
                 Cleanup();
             }
             finally
@@ -227,7 +280,60 @@ namespace IoT_Face_Reader
                 takePhoto.IsEnabled = true;
                 // recordVideo.IsEnabled = true;
             }
+
         }
+
+
+        public async void updateNMEA() // note that this doesn't work properly yet
+        {
+            //Create an HTTP client object
+            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+
+            //Add a user-agent header to the GET request. 
+            var headers = httpClient.DefaultRequestHeaders;
+
+            //The safe way to add a header value is to use the TryParseAdd method and verify the return value is true,
+            //especially if the header value is coming from user input.
+            string header = "ie";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+
+            header = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+
+            Uri requestUri = new Uri("http://192.168.0.100:50000/");
+
+            //Send the GET request asynchronously and retrieve the response as a string.
+            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
+            string httpResponseBody = "";
+
+            try
+            {
+                //Send the GET request
+            System.Threading.CancellationTokenSource source = new System.Threading.CancellationTokenSource(2000);
+            // client.GetAsync(new Uri("http://example.com")).AsTask(source.Token);
+            httpResponse = await httpClient.GetAsync(requestUri, Windows.Web.Http.HttpCompletionOption.ResponseHeadersRead).AsTask(source.Token);
+            httpResponse.EnsureSuccessStatusCode();
+            httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+            statusBox2.Text = httpResponseBody;
+            }
+            catch (TaskCanceledException exx)
+            {
+                statusBox2.Text = "foool: " + exx.ToString();
+            }
+            catch (Exception ex)
+            {
+                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
+            }
+        }
+
+
+
 
         /// <summary>
         /// Callback function for any failures in MediaCapture operations
@@ -263,10 +369,6 @@ namespace IoT_Face_Reader
 
 
 
-
-        private void ClickMe_Click(object sender, RoutedEventArgs e)
-        {
-            this.HelloMessage.Text = "Hello, Windows IoT Core!";
-        }
+        
     }
 }
