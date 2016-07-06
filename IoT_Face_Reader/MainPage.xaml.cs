5de7ad6287d3fb2bc7b6ce9fbbb4224acc5274fd
tyler <yostx038@umn.edu>
2016-07-06 13:48:51 -0500

    Multiple faces working

--- IoT_Face_Reader/MainPage.xaml.cs
+++ IoT_Face_Reader/MainPage.xaml.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
 using System.Threading;
-using System.Drawing;
-using System.Net;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
@@ -17,32 +10,40 @@ using Windows.Media.Capture;
 using Windows.Media.MediaProperties;
 using Windows.Storage;
 using Windows.Storage.Streams;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using Microsoft.ProjectOxford.Face;
 using Microsoft.ProjectOxford.Emotion;
 using Microsoft.ProjectOxford.Face.Contract;
-using System.Net.Http;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// Tyler's face-IoT project
+
 
 namespace IoT_Face_Reader
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
         // TAY's Face A free key from MSFT Cog Svcs.
-        private readonly IFaceServiceClient faceServiceClient = new FaceServiceClient("9725d03742394560be3ff295e1e435a2");
+        // private readonly IFaceServiceClient faceServiceClient = new FaceServiceClient("9725d03742394560be3ff295e1e435a2");
 
         private MediaCapture mediaCapture;
         private StorageFile photoFile;
         private readonly string PHOTO_FILE_NAME = "photo.jpg";
         private bool isPreviewing;
+
+        public Face[] faceResult;
+        public Microsoft.ProjectOxford.Emotion.Contract.Emotion[] emotionResult;
+
+
+
+        // int to keep track of which face to look at
+        public int numFaces = 0;
+        public int currentFace = 0;
+
+        public int imageWidth = 640;
+        public int imageHeight = 480;
+
+        public WriteableBitmap writeableBitmap;
+
 
         #region HELPER_FUNCTIONS
 
@@ -52,45 +53,31 @@ namespace IoT_Face_Reader
             DISABLE
         }
 
-        /// <summary>
-        /// Helper function to enable or disable Initialization buttons
-        /// </summary>
-        /// <param name="action">enum Action</param>
+
         private void SetInitButtonVisibility(Action action)
         {
             if (action == Action.ENABLE)
             {
                 video_init.IsEnabled = true;
-                // audio_init.IsEnabled = true;
             }
             else
             {
                 video_init.IsEnabled = false;
-                // audio_init.IsEnabled = false;
             }
         }
 
-        /// <summary>
-        /// Helper function to enable or disable video related buttons (TakePhoto, Start Video Record)
-        /// </summary>
-        /// <param name="action">enum Action</param>
+
         private void SetVideoButtonVisibility(Action action)
         {
             if (action == Action.ENABLE)
             {
                 takePhoto.IsEnabled = true;
                 takePhoto.Visibility = Visibility.Visible;
-
-                // recordVideo.IsEnabled = true;
-                // recordVideo.Visibility = Visibility.Visible;
             }
             else
             {
                 takePhoto.IsEnabled = false;
                 takePhoto.Visibility = Visibility.Collapsed;
-
-                // recordVideo.IsEnabled = false;
-                // recordVideo.Visibility = Visibility.Collapsed;
             }
         }
 
@@ -102,12 +89,14 @@ namespace IoT_Face_Reader
             SynchronizationContext syncContext = SynchronizationContext.Current;
 
             SetInitButtonVisibility(Action.ENABLE);
-            SetVideoButtonVisibility(Action.DISABLE);
-            // SetAudioButtonVisibility(Action.DISABLE);
-
-            // isRecording = false;
             isPreviewing = false;
+
+            writeableBitmap = new WriteableBitmap(imageWidth, imageHeight);
+            captureImage.Source = writeableBitmap;
+
         }
+
+
 
 
         private async void Cleanup()
@@ -119,15 +108,7 @@ namespace IoT_Face_Reader
                 {
                     await mediaCapture.StopPreviewAsync();
                     captureImage.Source = null;
-                    // playbackElement.Source = null;
                     isPreviewing = false;
-                }
-                if (1 == 2) // kludge I know
-                {
-                    await mediaCapture.StopRecordAsync();
-                    //isRecording = false;
-                    //recordVideo.Content = "Start Video Record";
-                    //recordAudio.Content = "Start Audio Record";
                 }
                 mediaCapture.Dispose();
                 mediaCapture = null;
@@ -138,11 +119,8 @@ namespace IoT_Face_Reader
 
         public async void initVideo_Click(object sender, RoutedEventArgs e)
         {
-            // Disable all buttons until initialization completes
 
             SetInitButtonVisibility(Action.DISABLE);
-            SetVideoButtonVisibility(Action.DISABLE);
-            // SetAudioButtonVisibility(Action.DISABLE);
 
             try
             {
@@ -156,16 +134,8 @@ namespace IoT_Face_Reader
                     {
                         await mediaCapture.StopPreviewAsync();
                         captureImage.Source = null;
-                        // playbackElement.Source = null;
                         isPreviewing = false;
                     }
-                    //if (isRecording)
-                    // {
-                    //     await mediaCapture.StopRecordAsync();
-                    //    isRecording = false;
-                    //    recordVideo.Content = "Start Video Record";
-                    //    recordAudio.Content = "Start Audio Record";
-                    //}
                     mediaCapture.Dispose();
                     mediaCapture = null;
                 }
@@ -178,7 +148,6 @@ namespace IoT_Face_Reader
                 // Set callbacks for failure and recording limit exceeded
                 statusBox.Text = "Device successfully initialized for video recording!";
                 mediaCapture.Failed += new MediaCaptureFailedEventHandler(mediaCapture_Failed);
-                // mediaCapture.RecordLimitationExceeded += new Windows.Media.Capture.RecordLimitationExceededEventHandler(mediaCapture_RecordLimitExceeded);
 
                 // Start Preview                
                 previewElement.Source = mediaCapture;
@@ -186,11 +155,6 @@ namespace IoT_Face_Reader
                 isPreviewing = true;
                 statusBox.Text = "Camera preview succeeded";
 
-                // Enable buttons for video and photo capture
-                SetVideoButtonVisibility(Action.ENABLE);
-
-                // Enable Audio Only Init button, leave the video init button disabled
-                // audio_init.IsEnabled = true;
             }
             catch (Exception ex)
             {
@@ -201,25 +165,18 @@ namespace IoT_Face_Reader
         private void cleanup_Click(object sender, RoutedEventArgs e)
         {
             SetInitButtonVisibility(Action.DISABLE);
-            SetVideoButtonVisibility(Action.DISABLE);
-            // SetAudioButtonVisibility(Action.DISABLE);
             Cleanup();
         }
 
-        /// <summary>
-        /// 'Take Photo' button click action function
-        /// Capture image to a file in the default account photos folderr
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+
+
+
         public async void takePhoto_Click(object sender, RoutedEventArgs e)
         {
 
             try
             {
                 takePhoto.IsEnabled = false;
-                // recordVideo.IsEnabled = false;
-                captureImage.Source = null;
 
                 photoFile = await KnownFolders.PicturesLibrary.CreateFileAsync(
                     PHOTO_FILE_NAME, CreationCollisionOption.GenerateUniqueName);
@@ -229,110 +186,147 @@ namespace IoT_Face_Reader
                 statusBox.Text = "Take Photo succeeded: " + photoFile.Path;
 
                 IRandomAccessStream photoStream = await photoFile.OpenReadAsync();
+                IRandomAccessStream photoStream2 = await photoFile.OpenReadAsync();
                 BitmapImage bitmap = new BitmapImage();
                 bitmap.SetSource(photoStream);
-                captureImage.Source = bitmap;
+                await writeableBitmap.SetSourceAsync(photoStream2);
 
                 // and now for the face API call
-                // FaceAttributes attribs = await UploadAndDetectFaces(photoFile.Path);
-                statusBox.Text = "00 ok just about to upload face";
-                // Emotion A key: c9306a1f134749759f1f4f9ae8838e1a
+                statusBox.Text = "Uploading image for Face API";
 
-                //var photoStreamNonrandom = new StreamReader(photoStream.AsStream());
-                Stream stream = photoStream.AsStream();
                 Stream fs1 = await photoFile.OpenStreamForReadAsync();
                 Stream fs2 = await photoFile.OpenStreamForReadAsync();
 
-                String faceURL = "http://i234.photobucket.com/albums/ee136/suwarnaadi/hair/Jason-Bateman-formal-hairstyle.jpg";
-
                 var faceClient = new FaceServiceClient("9725d03742394560be3ff295e1e435a2");
                 var emotionClient = new EmotionServiceClient("c9306a1f134749759f1f4f9ae8838e1a");
-                var result = await faceClient.DetectAsync(fs1);
-                var emotionResult = await emotionClient.RecognizeAsync(fs2);
+                faceResult = await faceClient.DetectAsync(fs1);
+                emotionResult = await emotionClient.RecognizeAsync(fs2);
 
-                // the above result returns a Face[], so...
-                ageBox.Text = result[0].FaceAttributes.Age.ToString();
-                genderBox.Text = result[0].FaceAttributes.Gender.ToString();
-                smileBox.Text = result[0].FaceAttributes.Smile.ToString();
-                // facialHairBox.Text = result[0].FaceAttributes.FacialHair.ToString();
-                glassesBox.Text = result[0].FaceAttributes.Glasses.ToString();
+                numFaces = faceResult.Length;
 
-                angerBox.Text = emotionResult[0].Scores.Anger.ToString();
-                contemptBox.Text = emotionResult[0].Scores.Contempt.ToString();
-                disgustBox.Text = emotionResult[0].Scores.Disgust.ToString();
-                fearBox.Text = emotionResult[0].Scores.Fear.ToString();
-                happinessBox.Text = emotionResult[0].Scores.Happiness.ToString();
-                neutralBox.Text = emotionResult[0].Scores.Neutral.ToString();
-                sadnessBox.Text = emotionResult[0].Scores.Sadness.ToString();
-                surpriseBox.Text = emotionResult[0].Scores.Surprise.ToString();
+                statusBox.Text = "Number of faces detected: " + numFaces.ToString();
+                currentFace = 0;
 
-                
-
+                if (numFaces > 0) // if faces were returned in the result, display the first one
+                {
+                    displayFaceInfo();
+                    displayImage();
+                }
 
             }
             catch (Exception ex)
             {
-                statusBox.Text = "foo: " + ex.Message;
+                statusBox.Text = ex.Message;
                 Cleanup();
             }
             finally
             {
                 takePhoto.IsEnabled = true;
-                // recordVideo.IsEnabled = true;
             }
 
         }
 
 
-        public async void updateNMEA() // note that this doesn't work properly yet
+        public async void displayImage()
         {
-            //Create an HTTP client object
-            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+            // displays the image
 
-            //Add a user-agent header to the GET request. 
-            var headers = httpClient.DefaultRequestHeaders;
+            var width = faceResult[currentFace].FaceRectangle.Width;
+            var height = faceResult[currentFace].FaceRectangle.Height;
+            var left = faceResult[currentFace].FaceRectangle.Left;
+            var top = faceResult[currentFace].FaceRectangle.Top;
 
-            //The safe way to add a header value is to use the TryParseAdd method and verify the return value is true,
-            //especially if the header value is coming from user input.
-            string header = "ie";
-            if (!headers.UserAgent.TryParseAdd(header))
+
+            using (IRandomAccessStream fileStream = await photoFile.OpenAsync(Windows.Storage.FileAccessMode.Read))
             {
-                throw new Exception("Invalid header value: " + header);
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(fileStream);
+
+                // Scale image to appropriate size
+                BitmapTransform transform = new BitmapTransform()
+                {
+                    ScaledWidth = Convert.ToUInt32(writeableBitmap.PixelWidth),
+                    ScaledHeight = Convert.ToUInt32(writeableBitmap.PixelHeight)
+                };
+
+                PixelDataProvider pixelData = await decoder.GetPixelDataAsync(
+                    BitmapPixelFormat.Bgra8,    // WriteableBitmap uses BGRA format
+                    BitmapAlphaMode.Straight,
+                    transform,
+                    ExifOrientationMode.IgnoreExifOrientation, // This sample ignores Exif orientation
+                    ColorManagementMode.DoNotColorManage);
+
+                byte[] sourcePixels = pixelData.DetachPixelData();
+
+                long coord = 0;
+
+                for (int x = (left * 4); x < ((left * 4) + 40); x+=4)
+                {
+                    for (int y = (top * 4); y < ((top * 4) + 40); y+=4)
+                    {
+                        coord = x  + (y * Convert.ToUInt32(writeableBitmap.PixelWidth));
+                        sourcePixels[coord + 0] = 0;
+                        sourcePixels[coord + 1] = 0;
+                        sourcePixels[coord + 2] = 255;
+                        sourcePixels[coord + 3] = 255;
+                    }
+                }
+
+                // Open a stream to copy the image contents to the WriteableBitmap's pixel buffer
+                using (Stream stream = writeableBitmap.PixelBuffer.AsStream())
+                {
+                    await stream.WriteAsync(sourcePixels, 0, sourcePixels.Length);
+                }
             }
 
-            header = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)";
-            if (!headers.UserAgent.TryParseAdd(header))
-            {
-                throw new Exception("Invalid header value: " + header);
-            }
+            // Redraw the WriteableBitmap
+            writeableBitmap.Invalidate();
 
-            Uri requestUri = new Uri("http://192.168.0.100:50000/");
-
-            //Send the GET request asynchronously and retrieve the response as a string.
-            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
-            string httpResponseBody = "";
-
-            try
-            {
-                //Send the GET request
-            System.Threading.CancellationTokenSource source = new System.Threading.CancellationTokenSource(2000);
-            // client.GetAsync(new Uri("http://example.com")).AsTask(source.Token);
-            httpResponse = await httpClient.GetAsync(requestUri, Windows.Web.Http.HttpCompletionOption.ResponseHeadersRead).AsTask(source.Token);
-            httpResponse.EnsureSuccessStatusCode();
-            httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
-            statusBox2.Text = httpResponseBody;
-            }
-            catch (TaskCanceledException exx)
-            {
-                statusBox2.Text = "foool: " + exx.ToString();
-            }
-            catch (Exception ex)
-            {
-                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
-            }
         }
 
 
+
+        public async void displayFaceInfo()
+        {
+            // first draw a box on the face we're looking at
+            ageBox.Text = faceResult[currentFace].FaceAttributes.Age.ToString();
+            genderBox.Text = faceResult[currentFace].FaceAttributes.Gender.ToString();
+            smileBox.Text = faceResult[currentFace].FaceAttributes.Smile.ToString();
+            glassesBox.Text = faceResult[currentFace].FaceAttributes.Glasses.ToString();
+
+            angerBox.Text = emotionResult[currentFace].Scores.Anger.ToString();
+            contemptBox.Text = emotionResult[currentFace].Scores.Contempt.ToString();
+            disgustBox.Text = emotionResult[currentFace].Scores.Disgust.ToString();
+            fearBox.Text = emotionResult[currentFace].Scores.Fear.ToString();
+            happinessBox.Text = emotionResult[currentFace].Scores.Happiness.ToString();
+            neutralBox.Text = emotionResult[currentFace].Scores.Neutral.ToString();
+            sadnessBox.Text = emotionResult[currentFace].Scores.Sadness.ToString();
+            surpriseBox.Text = emotionResult[currentFace].Scores.Surprise.ToString();
+
+            //last, update counter
+            faceNumBox.Text = currentFace.ToString();
+        }
+
+
+        public async void incrementFace_Click(object sender, RoutedEventArgs e)
+        {
+            if (numFaces <= 1) // only one face
+            {
+                statusBox.Text = "only one face detected";
+                return;
+            }
+            if (currentFace < (numFaces - 1)) // more than one face, currentFace is less than max
+            {
+                currentFace++;
+                displayFaceInfo();
+                displayImage();
+            }
+            if (currentFace == (numFaces - 1))
+            {
+                currentFace = 0;
+                displayFaceInfo();
+                displayImage();
+            }
+        }
 
 
         /// <summary>
---
+++ IoT_Face_Reader/gpsClass.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Drawing;
+using System.Net;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Face.Contract;
+using System.Net.Http;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
+
+public class gpsClass
+{
+
+    // serial port stuff
+    private SerialDevice serialPort = null;
+    DataWriter dataWriteObject = null;
+    DataReader dataReaderObject = null;
+
+    private ObservableCollection<DeviceInformation> listOfDevices;
+    private CancellationTokenSource ReadCancellationTokenSource;
+
+
+    public async void gpsInit()
+	{
+        // for serial init
+        // this.InitializeComponent();
+        // comPortInput.IsEnabled = true;
+        // sendTextButton.IsEnabled = false;
+        listOfDevices = new ObservableCollection<DeviceInformation>();
+        // ListAvailablePorts();
+        // ConnectDevices.SelectedIndex = -1;
+    }
+
+    private async void Listen()
+    {
+        try
+        {
+            if (serialPort != null)
+            {
+                dataReaderObject = new DataReader(serialPort.InputStream);
+
+                // keep reading the serial input
+                //while (true)
+                //{
+                await ReadAsync(ReadCancellationTokenSource.Token);
+                //}
+            }
+        }
+        catch (Exception ex)
+        {
+            if (ex.GetType().Name == "TaskCanceledException")
+            {
+                statusBox.Text = "Reading task was cancelled, closing device and cleaning up";
+                CloseDevice();
+            }
+            else
+            {
+                statusBox.Text = ex.Message;
+            }
+        }
+        finally
+        {
+            // Cleanup once complete
+            if (dataReaderObject != null)
+            {
+                dataReaderObject.DetachStream();
+                dataReaderObject = null;
+            }
+        }
+    }
+
+    // close gps port
+    private void CloseDevice()
+    {
+        if (ReadCancellationTokenSource != null)
+        {
+            if (!ReadCancellationTokenSource.IsCancellationRequested)
+            {
+                ReadCancellationTokenSource.Cancel();
+            }
+        }
+        //
+        // from orig
+        if (serialPort != null)
+        {
+            serialPort.Dispose();
+        }
+        serialPort = null;
+
+        comPortInput.IsEnabled = true;
+        // sendTextButton.IsEnabled = false;
+        // rcvdText.Text = "";
+        listOfDevices.Clear();
+    }
+
+    // read gps data async
+    private async Task ReadAsync(CancellationToken cancellationToken)
+    {
+        try
+        {
+            Task<UInt32> loadAsyncTask;
+
+            uint ReadBufferLength = 2048;
+            string gpsSentencePrefix = "blah";
+            string gpsSentenceTemp = "STUFF, things, carp, blah, blah, blah, balh, balh, thibngs";
+            string[] gpsSentence = gpsSentenceTemp.Split(',');
+
+            // $GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47
+
+            // If task cancellation was requested, comply
+            cancellationToken.ThrowIfCancellationRequested();
+
+            // Set InputStreamOptions to complete the asynchronous read operation when one or more bytes is available
+            dataReaderObject.InputStreamOptions = InputStreamOptions.Partial;
+
+            // Create a task object to wait for data on the serialPort.InputStream
+            loadAsyncTask = dataReaderObject.LoadAsync(ReadBufferLength).AsTask(cancellationToken);
+
+            // Launch the task and wait
+            UInt32 bytesRead = await loadAsyncTask;
+
+            StringReader strReader = new StringReader(dataReaderObject.ReadString(bytesRead));
+
+            //statusBox.Text = strReader.ReadLine() + "~~~~~" + strReader.ReadLine() + "zzzzzzzzz" + strReader.ReadLine();
+
+            while (gpsSentencePrefix != "$GPGGA")
+            {
+                //statusBox.Text = "rxd: " +  dataReaderObject.ReadString(bytesRead);
+                // 1 = time, 2 = lat, 4 = lon, 6=fixQuality, 7=n_sats
+                gpsSentenceTemp = strReader.ReadLine();
+                gpsSentence = gpsSentenceTemp.Split(',');
+                gpsSentencePrefix = gpsSentence[0];
+                if (gpsSentencePrefix == "$GPGGA")
+                {
+                    statusBox.Text = "lat = " + gpsSentence[2] + ", lon = " + gpsSentence[4] + ",  fix = " + gpsSentence[6] +
+                        ", sats = " + gpsSentence[7] + "\n" + gpsSentenceTemp;
+                }
+
+            }
+        }
+        catch (Exception ex)
+        {
+            statusBox.Text = ex.Message;
+        }
+
+
+        CloseDevice();
+    }
+
+
+    private async void ListAvailablePorts()
+    {
+        try
+        {
+            string aqs = SerialDevice.GetDeviceSelector();
+            var dis = await DeviceInformation.FindAllAsync(aqs);
+            listOfDevices.Add(dis[0]);
+
+            //statusBox.Text = "Select a device and connect";
+
+            //for (int i = 0; i < dis.Count; i++)
+            //{
+            //    listOfDevices.Add(dis[i]);
+            //}
+
+            // DeviceListSource.Source = listOfDevices;
+            comPortInput.IsEnabled = true;
+            //ConnectDevices.SelectedIndex = -1;
+        }
+        catch (Exception ex)
+        {
+            statusBox.Text = ex.Message;
+        }
+    }
+
+
+    private async void comPortInput_Click(object sender, RoutedEventArgs e)
+    {
+        ListAvailablePorts();
+        // var selection = ConnectDevices.SelectedItems;
+        var selection = listOfDevices;
+
+        //if (selection.Count <= 0)
+        // {
+        //   statusBox.Text = "Select a device and connect";
+        //    return;
+        //}
+
+        DeviceInformation entry = (DeviceInformation)selection[0];
+
+        try
+        {
+            serialPort = await SerialDevice.FromIdAsync(entry.Id);
+
+            // Disable the 'Connect' button 
+            comPortInput.IsEnabled = false;
+
+            // Configure serial settings
+            serialPort.WriteTimeout = TimeSpan.FromMilliseconds(1000);
+            serialPort.ReadTimeout = TimeSpan.FromMilliseconds(1000);
+            serialPort.BaudRate = 9600;
+            serialPort.Parity = SerialParity.None;
+            serialPort.StopBits = SerialStopBitCount.One;
+            serialPort.DataBits = 8;
+            serialPort.Handshake = SerialHandshake.None;
+
+            // Display configured settings
+            statusBox.Text = "Serial port configured successfully: ";
+            statusBox.Text += serialPort.BaudRate + "-";
+            statusBox.Text += serialPort.DataBits + "-";
+            statusBox.Text += serialPort.Parity.ToString() + "-";
+            statusBox.Text += serialPort.StopBits;
+
+            // Set the RcvdText field to invoke the TextChanged callback
+            // The callback launches an async Read task to wait for data
+            statusBox.Text = "Waiting for data...";
+
+            // Create cancellation token object to close I/O operations when closing the device
+            ReadCancellationTokenSource = new CancellationTokenSource();
+
+            // Enable 'WRITE' button to allow sending data
+            // sendTextButton.IsEnabled = true;
+
+            Listen();
+        }
+        catch (Exception ex)
+        {
+            statusBox.Text = ex.Message;
+            comPortInput.IsEnabled = true;
+            // sendTextButton.IsEnabled = false;
+        }
+    }
+
+
+}
